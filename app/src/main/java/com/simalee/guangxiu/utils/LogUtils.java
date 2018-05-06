689673dffc43ce56a4c41e3a173674e37a87dd4c
Sima Lee <programming_lgl@163.com>
2018-05-06 15:10:13 +0800

    add two packages

---
+++ app/src/main/java/com/simalee/guangxiu/utils/LogUtils.java
@@ -0,0 +1,8 @@
+package com.simalee.guangxiu.utils;
+
+/**
+ * Created by Lee Sima on 2018/5/6.
+ */
+
+public class LogUtils {
+}
---
+++ app/src/main/java/com/simalee/guangxiu/widget/MultiItemView.java
@@ -0,0 +1,8 @@
+package com.simalee.guangxiu.widget;
+
+/**
+ * Created by Lee Sima on 2018/5/6.
+ */
+
+public class MultiItemView {
+}
