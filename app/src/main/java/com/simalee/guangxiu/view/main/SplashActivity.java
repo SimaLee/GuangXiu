b6fc477b4ae6c5df3c7c8b4bf58902f3237efbdd
Sima Lee <programming_lgl@163.com>
2018-05-22 09:01:18 +0800

    新建启动页

---
+++ app/src/main/java/com/simalee/guangxiu/view/main/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.simalee.guangxiu.view.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lee Sima on 2018/5/22.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = "SplashActivity";
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
